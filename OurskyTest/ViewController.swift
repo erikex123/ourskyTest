29a1cf07dffebf3bf4aefe05c7d5995bfdb04beb
エリック <gtsang@Tsangs-MacBook.local>
2017-03-02 15:33:06 +0800

    Initial Commit

---
+++ OurskyTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OurskyTest
+//
+//  Created by エリック on 2017/03/02.
+//  Copyright © 2017年 エリック. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
